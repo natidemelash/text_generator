cd9781e8c6fe52e77985ac1f097d976c9636df4e
Nati_Gooday <natnael@gooday.io>
2024-03-15 08:08:22 +0300

    update validation for custom message component

--- src/view/Custom.vue
+++ src/view/Custom.vue
@@ -4,16 +4,20 @@
         <div class="flex flex-col w-[80%]">
            <label for="service">Job Dispatched</label> 
            <select class="bg-[#333] py-3 px-4 rounded-md" v-model="selectedService">
-            <option v-for="(service , index) in services" :key="index">{{ service }}</option>
+                <option disabled value="">Select Service</option>
+                <option v-for="(service , index) in services" :key="index" :value="service">{{ service }}</option>
            </select>
+           <div v-if="errors.selectedService" class="text-red-500">{{ errorMessage.selectedService }}</div>
         </div>
         <div class="my-2 flex flex-col w-[70%]">
             <label for="SP_name">SP name / የባለሙያ ስም</label>
             <input type="text" name="SP_name" v-model="serviceProviderName" placeholder="Service Provider name" class="py-2 px-3 bg-[#ECF0F1] text-black my-1 rounded focus:outline-none">
+            <div v-if="errors.serviceProviderName" class="text-red-500 ">{{ errorMessage.name }}</div>
         </div>
         <div class="mt-2 mb-6 flex flex-col w-[70%]">
             <label for="SP_phone">SP phone/የባለሙያ ስልክ ቁጥር</label>
             <input type="text" name="SP_phone" v-model="serviceProviderPhone" placeholder="Service Provider Phone number" class="py-2 px-3 bg-[#ECF0F1] text-black my-1 rounded focus:outline-none">
+            <div v-if="errors.serviceProviderPhone" class="text-red-500 w-3/4">{{ errorMessage.phone }}</div>
         </div>
 
         <button class="bg-[#e21e81] text-sm text-white px-4 py-2 rounded-full" @click="blsrMessage">Generate Msg</button>
@@ -102,18 +106,41 @@ export default {
             ],
             serviceProviderName: '',
             serviceProviderPhone: '',
-            message: ''
+            message: '',
+            errors:{
+                selectedService: false,
+                serviceProviderName: false,
+                serviceProviderPhone: false,
+            },
+            errorMessage: {
+                selectedService: "Please select a service.",
+                name: "Please enter the SP name.",
+                phone: "Please enter a valid phone number",
+            },
         }
     },
 
     methods:{
+        validateForm() {
+            let isValid = true;
+            this.errors.selectedService = !this.selectedService;
+            this.errors.serviceProviderName = !this.serviceProviderName;
+            this.errors.serviceProviderPhone = !this.isValidPhoneNumber(this.serviceProviderPhone);
+            isValid = !this.errors.selectedService && !this.errors.serviceProviderName && !this.errors.serviceProviderPhone;
+            return isValid;
+        },
+        isValidPhoneNumber(phone) {
+            const phoneRegex = /^(\+2519\d{8}|09\d{8})$/;
+            return phoneRegex.test(phone);
+        },
+
         blsrMessage() {
-             this.message = `የ${this.selectedService} ባለሙያ ከGoodayOn ወደ (${this.serviceProviderName}) በ(${this.serviceProviderPhone}) ይደውሉ። መልካም ቀን`;
-
-             this.serviceProviderName = '';
-             this.serviceProviderPhone = '';
-
-            return message;
+            if(this.validateForm()){
+                this.message = `የ${this.selectedService} ባለሙያ ከGoodayOn ወደ (${this.serviceProviderName}) በ(${this.serviceProviderPhone}) ይደውሉ። መልካም ቀን`;
+                this.serviceProviderName = '';
+                this.serviceProviderPhone = '';
+                return message;
+            }
         },
         copyToClipboard() {
             const el = document.createElement("textarea");
