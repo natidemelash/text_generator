748a57b62e318a5716f5149d4ffc25228167eb7f
NatnaelDemelash <natidemelash074@gmail.com>
2024-04-05 08:24:49 +0300

    changed colorcode status for NUE message

--- src/view/Engagement.vue
+++ src/view/Engagement.vue
@@ -64,7 +64,7 @@
   <div v-if="selectedProjectType === 'NUE'" class="card p-6 mx-3 md:mx-auto max-w-3xl bg-[#4f4d4d] mt-8 shadow-md rounded-md">
     <h3 class="text-lg mb-4 text-[#fff]">New User Engagement Message</h3>
     <div class="grid grid-cols-1 md:grid-cols-3 items-center text-black gap-8">
-      <button class="bg-[#e21e81] text-xs text-white px-6 py-3 rounded"  @click="handleButtonClick('nue-1')">Only App downloaded</button>
+      <button class="bg-[#A569BD] text-xs text-white px-6 py-3 rounded"  @click="handleButtonClick('nue-1')">New user(app downloaded) - not responsive</button>
     </div>
   </div>
 </template>
